72585df5f77b4348996387f9a0481b36673e0427
bugesz <kbugesz@gmail.com>
2016-11-19 18:32:53 +0100

    Added functions to Request class

--- PacketRouting/src/Request.java
+++ PacketRouting/src/Request.java
@@ -1,10 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Request {
 	
-	private int source, destination;
+	private int source, destination;	// Source and destination vertex
+	private int current;				// Current vertex
+	
+	private boolean over;				// Indicates if the packet has reached its destination
+	
+	// Arrays containing the init, sketch and final route for the package
+	private ArrayList<Integer> sketch, init, route;
+	
 	
 	public Request(int source, int destination) {
 		this.source = source;
 		this.destination = destination;
+		
+		current = source;
+		
+		over = false;
+		
+		init   = new ArrayList<Integer>();
+		route  = new ArrayList<Integer>();
+		sketch = new ArrayList<Integer>();
 	}
 	
 	public int distance() {
@@ -18,4 +36,47 @@ public class Request {
 	public int getDestination() {
 		return this.destination;
 	}
+	
+	public void move() {
+		int tmp = route.get(0);
+		
+		if(tmp == 1) {
+			current++;
+			
+			if(current == destination) {
+				over = true;
+			}
+		}
+		
+	}
+
+	public boolean isOver() {
+		return over;
+	}
+	
+	public void setInit(ArrayList<Integer> init) {
+		this.init = init;
+	}
+	
+	public void setSketch(ArrayList<Integer> sketch) {
+		this.sketch = sketch;
+	}
+	
+	public void setRoute(ArrayList<Integer> route) {
+		this.route = route;
+	}	
+
+	public ArrayList<Integer> getInit() {
+		return this.init;
+	}
+	
+	public ArrayList<Integer> getSketch() {
+		return this.sketch;
+	}
+	
+	public ArrayList<Integer> getRoute() {
+		return this.route;
+	}
+
+	
 }
